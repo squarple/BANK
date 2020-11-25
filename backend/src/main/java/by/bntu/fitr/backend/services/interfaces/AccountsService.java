2d70102177e8f844987a46925be30af6071665f5
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 18:08:46 +0300

    update AccountsService.

--- backend/src/main/java/by/bntu/fitr/backend/services/interfaces/AccountsService.java
+++ backend/src/main/java/by/bntu/fitr/backend/services/interfaces/AccountsService.java
@@ -1,4 +1,12 @@
 package by.bntu.fitr.backend.services.interfaces;
 
+import by.bntu.fitr.backend.entity.Accounts;
+import java.util.List;
+
 public interface AccountsService {
+    Accounts create(Accounts account);
+    Accounts findById(int id);
+    List<Accounts> findAll(int page, int size);
+    void deleteById(int id);
+    int getSize();
 }
