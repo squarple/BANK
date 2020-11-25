453590baee66e369ea48deb7c6b125571077be8f
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 19:57:26 +0300

    update PaymentsServiceImpl.

--- backend/src/main/java/by/bntu/fitr/backend/services/implementation/PaymentsServiceImpl.java
+++ backend/src/main/java/by/bntu/fitr/backend/services/implementation/PaymentsServiceImpl.java
@@ -1,4 +1,46 @@
 package by.bntu.fitr.backend.services.implementation;
 
-public class PaymentsServiceImpl {
+import by.bntu.fitr.backend.entity.Payments;
+import by.bntu.fitr.backend.repository.PaymentsRepo;
+import by.bntu.fitr.backend.services.interfaces.PaymentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class PaymentsServiceImpl implements PaymentsService {
+    @Autowired
+    private PaymentsRepo paymentRepo;
+
+    @Override
+    public Payments create(Payments payment) {
+        return paymentRepo.save(payment);
+    }
+
+    @Override
+    public Payments findById(int id) {
+        if (paymentRepo.findById(id).isPresent()) {
+            return paymentRepo.findById(id).get();
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public List<Payments> findAll(int page, int size) {
+        return paymentRepo.findAll(PageRequest.of(page, size)).getContent();
+    }
+
+    @Override
+    public void deleteById(int id) {
+        paymentRepo.deleteById(id);
+    }
+
+    @Override
+    public int getSize() {
+        return (int)paymentRepo.count();
+    }
 }
