0eff0bab4dad92a3ec88b10b0207b98a7865321d
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 20:19:41 +0300

    update AccountsController

--- backend/src/main/java/by/bntu/fitr/backend/controller/AccountsController.java
+++ backend/src/main/java/by/bntu/fitr/backend/controller/AccountsController.java
@@ -1,4 +1,42 @@
 package by.bntu.fitr.backend.controller;
 
+import by.bntu.fitr.backend.entity.Accounts;
+import by.bntu.fitr.backend.services.implementation.AccountsServiceImpl;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api
+@RestController
+@RequestMapping("/accounts")
 public class AccountsController {
+    @Autowired
+    private AccountsServiceImpl accountService;
+
+    @PostMapping
+    public Accounts create(@RequestBody Accounts forest){
+        return accountService.create(forest);
+    }
+
+    @GetMapping("/{id}")
+    public Accounts findById(@PathVariable int id) {
+        return accountService.findById(id);
+    }
+
+    @GetMapping("/find-all")
+    public List<Accounts> findAll(@RequestParam int pageNo, @RequestParam int pageSize){
+        return accountService.findAll(pageNo,pageSize);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteByID(@PathVariable int id){
+        accountService.deleteById(id);
+    }
+
+    @GetMapping("/size")
+    public int getSize() {
+        return accountService.getSize();
+    }
 }
