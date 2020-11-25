6a5383fd943db62f55e729220fe656efcc29882b
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 18:10:08 +0300

    update CardsService.

--- backend/src/main/java/by/bntu/fitr/backend/services/interfaces/CardsService.java
+++ backend/src/main/java/by/bntu/fitr/backend/services/interfaces/CardsService.java
@@ -1,4 +1,12 @@
 package by.bntu.fitr.backend.services.interfaces;
 
+import by.bntu.fitr.backend.entity.Cards;
+import java.util.List;
+
 public interface CardsService {
+    Cards create(Cards card);
+    Cards findById(int id);
+    List<Cards> findAll(int page, int size);
+    void deleteById(int id);
+    int getSize();
 }
