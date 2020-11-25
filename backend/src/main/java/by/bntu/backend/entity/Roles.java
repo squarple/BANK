01176a5dabbef6ab2f5bff044a54a4f0a28d1205
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 17:27:06 +0300

    update pom

--- backend/src/main/java/by/bntu/backend/entity/Roles.java
+++ backend/src/main/java/by/bntu/backend/entity/Roles.java
@@ -1,4 +1,23 @@
 package by.bntu.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Blob;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles" )
 public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer id;
+    
+    @Basic
+    @Column(name = "descr")
+    private String descr;
 }
