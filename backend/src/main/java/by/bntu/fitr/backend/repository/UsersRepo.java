158e3298ef5191a903cf6747dd5220db1def4e8d
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 17:59:13 +0300

    update UsersRepo.

--- backend/src/main/java/by/bntu/fitr/backend/repository/UsersRepo.java
+++ backend/src/main/java/by/bntu/fitr/backend/repository/UsersRepo.java
@@ -1,4 +1,11 @@
 package by.bntu.fitr.backend.repository;
 
-public interface UsersRepo {
+import by.bntu.fitr.backend.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepo extends JpaRepository<Users,Integer>{
+    Users findUsersByLogin (String login);
+    Users findUsersByLoginAndPassword (String login, String password);
 }
