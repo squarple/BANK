bf0243ca9405c7760479a7dc661e45532647ca5f
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 18:15:22 +0300

    update UsersService.

--- backend/src/main/java/by/bntu/fitr/backend/services/interfaces/UsersService.java
+++ backend/src/main/java/by/bntu/fitr/backend/services/interfaces/UsersService.java
@@ -1,4 +1,14 @@
 package by.bntu.fitr.backend.services.interfaces;
 
+import by.bntu.fitr.backend.entity.Users;
+import java.util.List;
+
 public interface UsersService {
+    Users create(Users user);
+    Users findById(int id);
+    List<Users> findAll(int page, int size);
+    void deleteById(int id);
+    int getSize();
+    Users findUserByLogin(String login);
+    Users findUsersByLoginAndPassword(String login, String password);
 }
