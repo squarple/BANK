2636cb9edfc2114ed4c33048588cde21644de886
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 18:17:07 +0300

    update BackApplication.

--- backend/src/main/java/by/bntu/fitr/backend/BackApplication.java
+++ backend/src/main/java/by/bntu/fitr/backend/BackApplication.java
@@ -1,4 +1,13 @@
 package by.bntu.fitr.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
 public class BackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackApplication.class, args);
+    }
 }
