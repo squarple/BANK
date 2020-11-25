fabbbd37c71004a1e8c13c57dc60d03a127db13e
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 17:53:10 +0300

    update PaymentsRepo.

--- backend/src/main/java/by/bntu/fitr/backend/repository/PaymentsRepo.java
+++ backend/src/main/java/by/bntu/fitr/backend/repository/PaymentsRepo.java
@@ -1,4 +1,9 @@
 package by.bntu.fitr.backend.repository;
 
-public interface PaymentsRepo {
+import by.bntu.fitr.backend.entity.Payments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentsRepo extends JpaRepository<Payments,Integer>{
 }
