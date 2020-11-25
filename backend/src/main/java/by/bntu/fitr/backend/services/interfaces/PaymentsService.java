ab9264be4edc04e47230065eedfe9ea3d8e98ca5
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 18:11:10 +0300

    update PaymentsService.

--- backend/src/main/java/by/bntu/fitr/backend/services/interfaces/PaymentsService.java
+++ backend/src/main/java/by/bntu/fitr/backend/services/interfaces/PaymentsService.java
@@ -1,4 +1,13 @@
 package by.bntu.fitr.backend.services.interfaces;
 
+import by.bntu.fitr.backend.entity.Payments;
+import java.util.List;
+
 public interface PaymentsService {
+    Payments create(Payments payment);
+    Payments findById(int id);
+    List<Payments> findAll(int page, int size);
+    void deleteById(int id);
+    int getSize();
+
 }
