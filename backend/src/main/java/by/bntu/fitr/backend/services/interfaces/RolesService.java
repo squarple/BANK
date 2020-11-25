c1f5c4153e558f0281f9494b801cb5999ce330ec
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 18:12:37 +0300

    update RolesService.

--- backend/src/main/java/by/bntu/fitr/backend/services/interfaces/RolesService.java
+++ backend/src/main/java/by/bntu/fitr/backend/services/interfaces/RolesService.java
@@ -1,4 +1,12 @@
 package by.bntu.fitr.backend.services.interfaces;
 
+import by.bntu.fitr.backend.entity.Roles;
+import java.util.List;
+
 public interface RolesService {
+    Roles create(Roles role);
+    Roles findById(int id);
+    List<Roles> findAll(int page, int size);
+    void deleteById(int id);
+    int getSize();
 }
