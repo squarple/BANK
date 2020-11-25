0ec5326ad84046e0047b2958f3c466013d182111
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 18:38:43 +0300

    update AccountsServiceImpl.

--- backend/src/main/java/by/bntu/fitr/backend/services/implementation/AccountsServiceImpl.java
+++ backend/src/main/java/by/bntu/fitr/backend/services/implementation/AccountsServiceImpl.java
@@ -1,4 +1,46 @@
 package by.bntu.fitr.backend.services.implementation;
 
-public class AccountsServiceImpl {
+import by.bntu.fitr.backend.entity.Accounts;
+import by.bntu.fitr.backend.repository.AccountsRepo;
+import by.bntu.fitr.backend.services.interfaces.AccountsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class AccountsServiceImpl implements AccountsService {
+    @Autowired
+    private AccountsRepo accountRepo;
+
+    @Override
+    public Accounts create(Accounts account) {
+        return accountRepo.save(account);
+    }
+
+    @Override
+    public Accounts findById(int id) {
+        if (accountRepo.findById(id).isPresent()) {
+            return accountRepo.findById(id).get();
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public List<Accounts> findAll(int page, int size) {
+        return accountRepo.findAll(PageRequest.of(page, size)).getContent();
+    }
+
+    @Override
+    public void deleteById(int id) {
+        accountRepo.deleteById(id);
+    }
+
+    @Override
+    public int getSize() {
+        return (int)accountRepo.count();
+    }
 }
