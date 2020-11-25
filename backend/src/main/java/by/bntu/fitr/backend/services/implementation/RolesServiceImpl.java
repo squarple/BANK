980d07d69ceb2c970a3bb8d01524af1e58437833
Anton Radionov <radionoff.antony@outlook.com>
2020-11-25 20:01:26 +0300

    update RolesServiceImpl.

--- backend/src/main/java/by/bntu/fitr/backend/services/implementation/RolesServiceImpl.java
+++ backend/src/main/java/by/bntu/fitr/backend/services/implementation/RolesServiceImpl.java
@@ -1,4 +1,46 @@
 package by.bntu.fitr.backend.services.implementation;
 
-public class RolesServiceImpl {
+import by.bntu.fitr.backend.entity.Roles;
+import by.bntu.fitr.backend.repository.RolesRepo;
+import by.bntu.fitr.backend.services.interfaces.RolesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class RolesServiceImpl implements RolesService {
+    @Autowired
+    private RolesRepo roleRepo;
+
+    @Override
+    public Roles create(Roles role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public Roles findById(int id) {
+        if (roleRepo.findById(id).isPresent()) {
+            return roleRepo.findById(id).get();
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public List<Roles> findAll(int page, int size) {
+        return roleRepo.findAll(PageRequest.of(page, size)).getContent();
+    }
+
+    @Override
+    public void deleteById(int id) {
+        roleRepo.deleteById(id);
+    }
+
+    @Override
+    public int getSize() {
+        return (int)roleRepo.count();
+    }
 }
